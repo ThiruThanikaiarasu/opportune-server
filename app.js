b74c826e0c970f9e1c8857ad4fdcc3a03f4a8f4d
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2025-01-02 17:17:05 +0530

    ADD - Configure Swagger UI for API documentation

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./configurations/swaggerConfig');
+
 app.get('/', (request, response) => {
     response.status(200).send({ message: "Server running successfully."})
 })
+
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
 
 module.exports = app
