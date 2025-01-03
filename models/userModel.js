7cbaf5a312ef0d1ff0adb50ddb84d2e0f0de8350
Sudharsananuma23@gmail.com <Sudhar2303>
2025-01-03 14:24:27 +0530

    UPDATE - update user model with hashing password

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
+const bcrypt = require('bcrypt')
 /**
  * @swagger
  *  components:
@@ -118,5 +119,21 @@ const userSchema = new mongoose.Schema(
         collection: 'users'
     }
 )
+
+userSchema.pre('save', function (next) {
+    const user = this
+
+    if (!user.isModified('password')) return next()
+    bcrypt.genSalt(10, (error, salt) => {
+        if (error) return next(error)
+
+        bcrypt.hash(user.password, salt, (error, hash) => {
+            if (error) return next(error)
+
+            user.password = hash
+            next()
+        })
+    })
+})
 
 module.exports = mongoose.model('users', userSchema)
