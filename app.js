4614089f83bea0434d5b5c482846053eccdbe057
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2025-01-12 14:55:15 +0530

    upgrade: add custom css url for swagger

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const authRoute = require('./routes/authRoute')
 const projectRoute = require('./routes/projectRoute')
 const userRoute = require('./routes/userRoute')
 const githubAuthRoute = require('./routes/githubAuthRoute')
+const { CSS_URL } = require('./configurations/constants')
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN_URL, 
@@ -27,7 +28,7 @@ app.get('/', (request, response) => {
     response.status(200).send({ message: "Server running successfully."})
 })
 
-app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {customCssUrl: CSS_URL}))
 app.use('/api/v1/auth',authRoute)
 app.use('/api/v1/project', projectRoute)
 app.use('/api/v1/user', userRoute)
---
+++ configurations/constants.js
@@ -0,0 +1,5 @@
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css"
+
+module.exports = {
+    CSS_URL
+}
